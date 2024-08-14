cd2bddcbfd7392c40e52bd243f6ea68dd7ffa8f9
Vivek <viveksingh.27@yahoo.com>
2024-08-14 23:05:03 +0530

    vercel Upd

--- server/controllers/ResetPassword.js
+++ server/controllers/ResetPassword.js
@@ -25,7 +25,7 @@ exports.resetPasswordToken = async (req, res) => {
 		);
 		console.log("DETAILS", updatedDetails);
 
-		const url = `https://online-course-portal-gules.vercel.app/update-password/${token}`;
+		const url = `https://study-point-ebon.vercel.app/update-password/${token}`;
 
 		await mailSender(
 			email,
